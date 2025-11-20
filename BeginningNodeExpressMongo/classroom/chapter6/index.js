90dd19133e5e4d3b70fb81e9e5dd7b53e6169a57
James1oliveira <oliveira1james@gmail.com>
2025-11-20 15:09:34 +0200

    update

--- BeginningNodeExpressMongo/classroom/chapter6/index.js
+++ BeginningNodeExpressMongo/classroom/chapter6/index.js
@@ -1,33 +1,56 @@
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
-app.set('view engine', 'ejs')
-const path = require('path')
+const express = require("express");
+const app = new express();
+const path = require("path");
 
-app.use(express.static('public'))
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_database', {
-  useNewUrlParser:
-    true
-})
+const ejs = require("ejs");
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
+// ==================== CHAPTER05 =======================
+const BlogPost = require("./models/BlogPost.js");
+// ======================================================
 
-app.get('/about', (req, res) => {
-  //res.sendFile(path.resolve(__dirname,'pages/about.html'))
-  res.render('about');
-})
-app.get('/contact', (req, res) => {
-  //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
-  res.render('contact');
-})
-app.get('/post', (req, res) => {
-  //res.sendFile(path.resolve(__dirname,'pages/post.html'))
-  res.render('post')
-})
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/my_database", { useNewUrlParser: true });
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "public", "views"));
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.listen(4000, () => {
-  console.log('App listening on port 4000')
-})
+  console.log("App listening on port 4000");
+});
+
+app.get("/", async (req, res) => {
+  const blogposts = await BlogPost.find({});
+  // console.log(blogposts);
+  res.render("index", {
+    blogposts,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/post/:id", async (req, res) => {
+  const blogpost = await BlogPost.findById(req.params.id);
+  res.render("post", {
+    blogpost,
+  });
+});
+
+app.get("/newPost", (req, res) => {
+  res.render("create");
+});
+
+app.post("/posts/store", async (req, res) => {
+  // model creates a new doc with browser data
+  await BlogPost.create(req.body);
+  res.redirect("/");
+});
